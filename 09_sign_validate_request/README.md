62b5c42f7036dd0e7cdc58b207e357a829c8ad16
Mike <3ddabbler@gmail.com>
2020-09-24 11:25:29 -0400

    finished code for GS 09

--- 09_sign_validate_request/README.md
+++ 09_sign_validate_request/README.md
@@ -1,3 +1,51 @@
 
 # Getting Started with Envoy & Open Policy Agent --- 08 ---
 ## Learn how to configure Envoy's access logs, taps for capturing full requests & responses and traces for capturing end to end flow
+
+
+``` bash
+curl --location --request POST 'localhost:8000/anything' \
+>         --header 'Content-Type: application/json' \
+>         --header 'Accept: application/json' \
+>         --header 'Actor-Token: some_user' \
+>         --header 'App-Token: some_app' \
+>         --header 'Subject-Token: some_subject' \
+>         --header 'Session-Id: 0001' \
+>         --header 'Request-Id: 0001' \
+>         --data-raw "{\"body-key\":\"body-value\"}"
+```
+
+``` json
+{
+  "args": {}, 
+  "data": "{\"body-key\":\"body-value\"}", 
+  "files": {}, 
+  "form": {}, 
+  "headers": {
+    "Accept": "application/json", 
+    "Actor-Token": "some_user", 
+    "App-Token": "some_app", 
+    "Content-Length": "25", 
+    "Content-Type": "application/json", 
+    "Digest": "...", 
+    "Host": "localhost:8000", 
+    "Request-Id": "0001", 
+    "Session-Id": "0001", 
+    "Subject-Token": "some_subject", 
+    "User-Agent": "curl/7.61.0", 
+    "Valid-Request": "true", 
+    "X-B3-Parentspanid": "8f0ca43d8048a62d", 
+    "X-B3-Sampled": "1", 
+    "X-B3-Spanid": "6bfec515a48ab6f7", 
+    "X-B3-Traceid": "8f0ca43d8048a62d", 
+    "X-Envoy-Expected-Rq-Timeout-Ms": "15000", 
+    "X-Envoy-Internal": "true"
+  }, 
+  "json": {
+    "body-key": "body-value"
+  }, 
+  "method": "POST", 
+  "origin": "172.27.0.1", 
+  "url": "http://localhost:8000/anything"
+}
+```
--- 09_sign_validate_request/demonstrate_sign_verify.sh
+++ 09_sign_validate_request/demonstrate_sign_verify.sh
@@ -1,17 +1,18 @@
 #!/bin/bash
 
-printf "\n\n**************    Demostration of configuring log fields, taps and traces    **************\n\n"
-printf "\n\n    This script starts an envoy front proxy and exposes it on localhost:8080.\n"
-printf "    Envoy is configured to forward traffic to service 1 and it in turn calls service 2.\n"
+printf "\n\n**************    Demostration of signing and verifying signed requests using OPA    **************\n\n"
+printf "\n\n    This script starts an envoy front proxy and exposes it on localhost:8000.\n"
+printf "    The Envoy front proxy is configured to forward traffic to Envoy service 1 and it in turn calls the App.\n"
+printf "    Signatures will still work if the signed headers and body are missing or empty.\n"
+printf "    In both cases it prevents someone from adding values to missing or empty fields.\n"
 
-printf "\n\n**************    About to remove temporary files from previous run (if any)    **************\n\n"
+printf "\n\n**************    About to remove temporary logs and taps files from previous run (if any)    **************\n\n"
 read -n 1 -r -s -p $'\n\nPress enter to continue...\n'
 rm -Rf tmp/
 mkdir -p tmp/service1
-mkdir -p tmp/service2
 mkdir -p tmp/front
 
-printf "\n\n**************    Starting envoy, service 1 and service 2 via docker-compose   **************\n\n"
+printf "\n\n**************    Starting the environment via docker-compose   **************\n\n"
 read -n 1 -r -s -p $'\n\nPress enter to continue...\n'
 
 docker-compose up -d --build
@@ -21,38 +22,83 @@ printf "\n\n**************    About to check to make sure everything is started 
 read -n 1 -r -s -p $'\n\nPress enter to continue...\n'
 docker ps -a
 
+
+# All signed attributes are missing
 read -n 1 -r -s -p $'\n\nPress enter to continue...\n'
-printf "\n\n**************    About to call service 1 via Envoy to start a trace   **************\n\n"
+printf "\n\n**************    Sending a request with all signed fields missing.   **************\n\n"
 read -n 1 -r -s -p $'Press enter to continue...\n'
-curl --location --request GET 'localhost:8000/trace/1' 
+curl --location --request POST 'localhost:8000/anything' \
+        --header 'Content-Type: application/json' \
+        --header 'Accept: application/json' 
+
+
+# All signed attributes empty
+read -n 1 -r -s -p $'\n\nPress enter to continue...\n'
+printf "\n\n**************    Sending a request with all signed fields empty.   **************\n\n"
+read -n 1 -r -s -p $'Press enter to continue...\n'
+curl --location --request POST 'localhost:8000/anything' \
+        --header 'Content-Type: application/json' \
+        --header 'Accept: application/json' \
+        --header 'Actor-Token:' \
+        --header 'App-Token:' \
+        --header 'Subject-Token:' \
+        --header 'Session-Id:' \
+        --header 'Request-Id:' \
+        --data-raw ""
+
+
+# All signed attributes are populated
+read -n 1 -r -s -p $'\n\nPress enter to continue...\n'
+printf "\n\n**************    Sending a request with all signed fields populated.   **************\n\n"
+read -n 1 -r -s -p $'Press enter to continue...\n'
+
+curl --location --request POST 'localhost:8000/anything' \
+        --header 'Content-Type: application/json' \
+        --header 'Accept: application/json' \
+        --header 'Actor-Token: some_user' \
+        --header 'App-Token: some_app' \
+        --header 'Subject-Token: some_subject' \
+        --header 'Session-Id: 0001' \
+        --header 'Request-Id: 0001' \
+        --data-raw "{\"body-key\":\"body-value\"}"
+
 
 read -n 1 -r -s -p $'\n\nPress enter to continue...\n'
 printf "\n\n*********************************************************************************************************\n\n"
-printf "\n\n**************                              CUSTOM LOG FORMAT                              **************\n\n"
+printf "\n\n**************                              Displaying LOGs                                **************\n\n"
 printf "\n\n*********************************************************************************************************\n\n"
-printf "\n\n**************    About to show the logs for the Front Proxy Envoy in our new custom format   **************\n\n"
+printf "\n\n**************    About to show the logs for the Front Proxy Envoy                         **************\n\n"
 read -n 1 -r -s -p $'Press enter to continue...\n'
 
-docker logs 08_log_taps_traces_front-envoy_1 1>tmp/front_envoy_stdOut.log 2>tmp/front_envoy_stdErr.log
+docker logs 09_sign_validate_request_front-envoy_1 1>tmp/front_envoy_stdOut.log 2>tmp/front_envoy_stdErr.log
 cat tmp/front_envoy_stdOut.log | docker run --rm -i imega/jq '.' --sort-keys && printf "\n\n"
 
 read -n 1 -r -s -p $'\n\nPress enter to continue...\n'
-printf "\n\n**************    About to show the logs for service 1's Envoy in our new custom format   **************\n\n"
+printf "\n\n**************    About to show the Open Policy Agent logs for signing the request         **************\n\n"
 read -n 1 -r -s -p $'Press enter to continue...\n'
-docker logs 08_log_taps_traces_service1_1 1>tmp/service1_envoy_stdOut.log 2>tmp/service1_envoy_stdErr.log
+docker logs 09_sign_validate_request_sign_1 1>tmp/signing_opa_stdOut.log 2>tmp/signing_opa_stdErr.log
+cat tmp/signing_opa_stdErr.log | docker run --rm -i imega/jq '.' --sort-keys && printf "\n\n"
+
+
+read -n 1 -r -s -p $'\n\nPress enter to continue...\n'
+printf "\n\n**************    About to show the logs for service 1's Envoy                             **************\n\n"
+read -n 1 -r -s -p $'Press enter to continue...\n'
+docker logs 09_sign_validate_request_service1_1 1>tmp/service1_envoy_stdOut.log 2>tmp/service1_envoy_stdErr.log
 cat tmp/service1_envoy_stdOut.log | docker run --rm -i imega/jq '.' --sort-keys && printf "\n\n"
 
 read -n 1 -r -s -p $'\n\nPress enter to continue...\n'
-printf "\n\n**************    About to show the logs for service 2's Envoy in our new custom format   **************\n\n"
+printf "\n\n**************    About to show the Open Policy Agent logs for verifying the request         **************\n\n"
 read -n 1 -r -s -p $'Press enter to continue...\n'
-docker logs 08_log_taps_traces_service2_1 1>tmp/service2_envoy_stdOut.log 2>tmp/service2_envoy_stdErr.log
-cat tmp/service2_envoy_stdOut.log | docker run --rm -i imega/jq '.' --sort-keys && printf "\n\n"
+docker logs 09_sign_validate_request_verify_1 1>tmp/verifying_opa_stdOut.log 2>tmp/verifying_opa_stdErr.log
+cat tmp/verifying_opa_stdErr.log | docker run --rm -i imega/jq '.' --sort-keys && printf "\n\n"
+
+
 
 printf "\n\n*********************************************************************************************************\n\n"
-printf "\n\n**************                        TAPS for REQUESTs & RESPONSES                        **************\n\n"
+printf "\n\n**************                   Displaying TAPS for REQUESTs & RESPONSES                  **************\n\n"
 printf "\n\n*********************************************************************************************************\n\n"
 
-printf "\n\n**************    About to show the Request / Response TAPS for the Front Proxy Envoy in our new custom format   **************\n\n"
+printf "\n\n**************    About to show the Request / Response TAPS for the Front Proxy Envoy      **************\n\n"
 read -n 1 -r -s -p $'Press enter to continue...\n'
 
 ls -alGh tmp/front
@@ -69,7 +115,7 @@ for file in tmp/front/*; do
 done
 
 read -n 1 -r -s -p $'\n\nPress enter to continue...\n'
-printf "\n\n**************    About to show the Request / Response TAPS for service 1's Envoy in our new custom format   **************\n\n"
+printf "\n\n**************    About to show the Request / Response TAPS for service 1's Envoy          **************\n\n"
 read -n 1 -r -s -p $'Press enter to continue...\n'
 
 ls -alGh tmp/service1
@@ -85,25 +131,8 @@ for file in tmp/service1/*; do
     read -n 1 -r -s -p $'Press enter to continue...\n'
 done
 
-read -n 1 -r -s -p $'\n\nPress enter to continue...\n'
-printf "\n\n**************    About to show the Request / Response TAPS for service 2's Envoy in our new custom format   **************\n\n"
-read -n 1 -r -s -p $'Press enter to continue...\n'
-
-ls -alGh tmp/service2
-read -n 1 -r -s -p $'Press enter to continue...\n'
-
-for file in tmp/service2/*; do 
-    printf "\n\n**************    Displaying Tap    **************\n\n"
-    printf "\n\n ...File: ${i} \n\n"
-    cat $file
-    read -n 1 -r -s -p $'Press enter to continue...\n'
-    printf "\n\n**************    decoding response body   **************\n\n"
-    cat $file | docker run --rm -i imega/jq -r '.http_buffered_trace.response.body.as_bytes' | base64 -d && printf "\n\n"
-    read -n 1 -r -s -p $'Press enter to continue...\n'
-done
-
 printf "\n\n*********************************************************************************************************\n\n"
-printf "\n\n**************                                    TRACES                                   **************\n\n"
+printf "\n\n**************                              Displaying TRACES                              **************\n\n"
 printf "\n\n*********************************************************************************************************\n\n"
 read -n 1 -r -s -p $'\n\nPress enter to continue...\n'
 printf "\n\n**************    Open a web browser to show the traces in Jaeger    **************\n\n"
